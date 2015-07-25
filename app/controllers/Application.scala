b70fd7e5b298611529d8d500a6a50ca46d4137f0
Ashwini <ashwini.rudrappa@gmail.com>
2015-07-25 19:41:39 +0100

    -m"commit for l-001"

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,7 +9,7 @@ class Application extends Controller {
   def index = Action {
     //Ok(views.html.index("Your new application is ready."))
 
-    //Add comment   branch l-001
+    //Add comment   branch l-001  modified on this brnch
     Ok(JsObject(Seq("message" ->JsString("It Works!"))))
   }
 
